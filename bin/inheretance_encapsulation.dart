54b6893b2a4ceb02bab71cffe95a02715208b859
MiralEmad <moraemadsamir700@gmail.com>
2026-08-02 16:02:22 +0300

    first commit

---
+++ bin/inheretance_encapsulation.dart
@@ -0,0 +1,5 @@
+import 'package:inheretance_encapsulation/inheretance_encapsulation.dart' as inheretance_encapsulation;
+
+void main(List<String> arguments) {
+  print('Hello world: ${inheretance_encapsulation.calculate()}!');
+}
---
+++ test/inheretance_encapsulation_test.dart
@@ -0,0 +1,8 @@
+import 'package:inheretance_encapsulation/inheretance_encapsulation.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
